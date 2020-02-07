ac960cfd1def2e6ed4b34937a80f0273f89706bd
MobileDev418 <oncemoreforyou@outlook.com>
2020-02-08 02:57:03 +0800

    android project

---
+++ android/app/src/main/java/com/myhabit/MainApplication.java
@@ -0,0 +1,66 @@
+package com.myhabit;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import org.reactnative.camera.RNCameraPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNCameraPackage(),
+            new FBSDKPackage(mCallbackManager),
+            new RNGestureHandlerPackage(),
+            new LinearGradientPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    AppEventsLogger.activateApp(this);
+    // SoLoader.init(this, /* native exopackage */ false);
+  }
+}
