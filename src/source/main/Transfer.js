86c06e46f9a51e50b099f42c9686c60eafa9b239
softdev8 <oncemoreforyou@outlook.com>
2020-02-11 22:24:26 +0800

    transfer

---
+++ src/source/main/Transfer.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+import { color } from "../styles/theme"
+
+export default class Transfer extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+    
+    openMenu = () => {
+        this.props.navigation.openDrawer(); 
+    }
+
+    render() {
+
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity onPress={() => this.openMenu()} >
+                    <Text style={{fontSize: 25}}>Money Transfer</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: color.white,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
